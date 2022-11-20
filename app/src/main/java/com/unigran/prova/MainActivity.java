ac7e94bccad17614a22ff880e942584d24c1fdc6
Ronan Laurindo Flor <ronanlf12@gmail.com>
2022-11-20 04:13:03 -0400

    finalização de sincronismo com firebase

--- app/src/main/java/com/unigran/prova/MainActivity.java
+++ app/src/main/java/com/unigran/prova/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
